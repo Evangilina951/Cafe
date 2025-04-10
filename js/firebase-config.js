60068c33b10f4d9284da0e3006052b1e17959207
Evangilina951 <0457376@mail.ru>
2025-04-11 02:14:16 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,18 @@
+// Конфигурация Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyDB8Vtxg3SjVyHRJ3ZOXT8osnHYrO_uw4A",
+    authDomain: "cafe-90de8.firebaseapp.com",
+    databaseURL: "https://cafe-90de8-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "cafe-90de8",
+    storageBucket: "cafe-90de8.appspot.com",
+    messagingSenderId: "1086414728245",
+    appId: "1:1086414728245:web:fbbec8b3adf4eba659957c",
+    measurementId: "G-2FVD2KRF16"
+};
 
+// Инициализация Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.database();
+
+export { auth, db };
