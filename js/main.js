a02a400fa1afb74312b4a874abd6ec4d81c7387b
Evangilina951 <0457376@mail.ru>
2025-04-14 21:05:36 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -31,10 +31,11 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     // Загрузка меню при изменении состояния аутентификации
-   auth.onAuthStateChanged(user => {
-    console.log('Auth state changed:', user);
-    if (user) {
-        console.log('User logged in, loading menu...');
-        loadMenuFromFirebase();
-    }
-});
+    auth.onAuthStateChanged(user => {
+        console.log('Auth state changed:', user);
+        if (user) {
+            console.log('User logged in, loading menu...');
+            loadMenuFromFirebase();
+        }
+    });
+}); 
